d6cd6444777fcc387e6cca4b9acabfe33c83abb6
Changsik Yoon <dbs2636@gmail.com>
2022-03-27 23:23:28 +0900

    feat: Add log lines with signale

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,19 @@
-import log from "signale";
+import { info, start, complete, star, error } from "signale";
 import { prompt } from "inquirer";
 
+const DEFAULT_CONTENTS_ADDRESS = "/content/blog";
+
+const getCategory = async () => {};
+
 export default (async () => {
-  console.log("hello world!");
+  info("Run gatsby-blog-post-gen...");
+  start(`Let's create a new Blog Post!`);
+
+  error("Something's wrong");
+
+  complete("Success to generate blog post frame!");
+  star("Go write your post!");
+
+  console.log("process :>> ", process.cwd());
+  console.log("process.argv", process.argv);
 })();
