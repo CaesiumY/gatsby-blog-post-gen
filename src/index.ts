c7efed51c923e3f4b59af8449aae50fc747059a6
Changsik Yoon <dbs2636@gmail.com>
2022-03-28 00:16:58 +0900

    feat: Add getTitle

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,39 @@
 import { info, start, complete, star, error } from "signale";
 import { prompt } from "inquirer";
+import { existsSync } from "fs";
 
 const DEFAULT_CONTENTS_ADDRESS = "/content/blog";
 
-const getCategory = async () => {};
+const getTitle = async (target: string) => {
+  const answer = await prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "Enter the post title: ",
+      default: () => `Other`,
+      validate: async (value: string) => {
+        if (value.includes("'")) return "Cannot use single quote";
+
+        const sanitizedValue = value.toLowerCase().replace(/ +/g, "-");
+        const fileName = `${target}/${sanitizedValue}.mdx`;
+
+        const isAlreadyExist = existsSync(fileName);
+
+        if (isAlreadyExist) error("File is Already Exists! Please try again.");
+
+        return true;
+      },
+    },
+  ]);
+
+  return answer.title;
+};
 
 export default (async () => {
   info("Run gatsby-blog-post-gen...");
   start(`Let's create a new Blog Post!`);
 
-  error("Something's wrong");
+  const title = await getTitle("");
 
   complete("Success to generate blog post frame!");
   star("Go write your post!");
